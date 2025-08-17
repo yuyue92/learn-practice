9ad4330eb664f62d8571d13dbfa1e5a33d04d83b
yuyue3 <yy18825237023@126.com>
2025-08-17 20:35:46 +0800

    Add files via upload

---
+++ vue-formDesigner/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // console.log('yy3 ', id);
+          if(id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
+  // base: '/autoDeploy/'
+  base: process.env.NODE_ENV === 'production' 
+    ? '/autoDeploy/'  // 替换为你的仓库名
+    : '/',
+})
