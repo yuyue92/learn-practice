f45100e2ae6b8dac30644aa26b2df5c0c1ce735a
yuyue3 <yy18825237023@126.com>
2025-08-26 16:19:17 +0800

    Add files via upload

--- next_ts_app/app/dashboard/page.tsx
+++ next_ts_app/app/dashboard/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+import { useState } from "react"
+
 export default function DashboardPage() {
+    const [posts, setposts] = useState([]);
+    async function getPosts() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+        return res.json()
+    }
+    const handleClickGET = async() => {
+        const result = await getPosts()
+        setposts(result)
+        console.log('resss: ', result )
+    }
     return (
         <section className="space-y-4">
             <h2 className="text-xl font-semibold">Dashboard</h2>
@@ -12,6 +25,18 @@ export default function DashboardPage() {
                     <div className="mt-2 text-3xl font-bold">98</div>
                 </div>
             </div>
+
+            <div className="border p-2">
+                <h1 className="border-b-2">Latest Posts
+                    <button onClick={ handleClickGET } className="bg-blue-600 text-white p-2 m-2 rounded">getData</button>
+                </h1>
+                <ul>
+                    {posts.slice(0, 10).map((p: any) => (
+                        <li key={p.id}>{p.title}</li>
+                    ))}
+                </ul>
+            </div>
+
         </section>
     )
 }
