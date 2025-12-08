68efd8edf59242ed646e0b606c5651a950e38fd0
yuyue3 <yy18825237023@126.com>
2025-12-08 17:05:15 +0800

    new add userStore

--- next_ts_app/app/users/page.tsx
+++ next_ts_app/app/users/page.tsx
@@ -1,12 +1,17 @@
+"use client"
+import { useUserInfoStore } from "store/useStore1";
 export default function UsersPage() {
-    const rows = [
-        { id: 1, name: 'Alice', role: 'Admin' },
-        { id: 2, name: 'Bob', role: 'Editor' },
-        { id: 3, name: 'Cara', role: 'Viewer' }
-    ]
+    const { userList, addOnUser, deleteUser } = useUserInfoStore();
+
+    const handleAddUser = () => {
+        addOnUser()
+    }
     return (
         <section>
-            <h2 className="mb-4 text-xl font-semibold">Users</h2>
+            <h2 className="mb-4 text-xl font-semibold">
+                <span>User-list: {userList.length}</span>
+                <button onClick={handleAddUser} className="bg-blue-600 text-white p-2 m-2 rounded">AddUser</button>
+            </h2>
             <div className="overflow-hidden rounded-2xl border bg-white shadow-sm">
                 <table className="min-w-full table-fixed">
                     <thead>
@@ -14,14 +19,16 @@ export default function UsersPage() {
                             <th className="p-3">ID</th>
                             <th className="p-3">Name</th>
                             <th className="p-3">Role</th>
+                            <th className="p-3">Operation</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {rows.map((r) => (
+                        {userList.map((r) => (
                             <tr key={r.id} className="border-t">
                                 <td className="p-3">{r.id}</td>
                                 <td className="p-3">{r.name}</td>
                                 <td className="p-3">{r.role}</td>
+                                <td className="p-3"><button onClick={() => deleteUser(r.id)} className="bg-blue-600 text-white p-1 m-1 rounded">delete</button></td>
                             </tr>
                         ))}
                     </tbody>
